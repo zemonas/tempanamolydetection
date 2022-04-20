6c405109fd63a3f98667ac8b9c34d5acc86172a4
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-04-21 02:40:29 +0530

    Temperature chart done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 import "./App.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
+import Temp from "./Components/Temp/Temp";
+import { useState, useEffect } from "react";
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://sheet.best/api/sheets/5b593795-c7a5-415b-89f7-c77f9915b119"
+    );
+    const data = await response.json();
+    setData(data);
+    console.log(data);
+  };
+  return (
+    <div className="App">
+      <div className="Header">
+        <h1>IOT App</h1>
+      </div>
+      <div className="Content">
+        {/* temp Humidity box */}
+        <div className="temp-humidity">
+          {/* temp box */}
+          <div className="temp-container">
+            <h3>{25.8} ˚C</h3>
+          </div>
+          {/* humidity box  */}
+          <div className="Humidity-container">
+            <h3>
+              {39} H<sub>A</sub>
+            </h3>
+          </div>
+        </div>
+
+        {/* temp echart */}
+        <div className="echart">
+          <Temp data={data} />
+        </div>
+        {/* humidity echart */}
+        <div className="echart">
+          <Temp data={data} />
+        </div>
+
+        {/* reload */}
+        <div className="reload">
+          <FontAwesomeIcon icon={faRotate} className="icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Temp/Temp.js
@@ -0,0 +1,66 @@
+import React from "react";
+import moment from "moment";
+import ReactECharts from "echarts-for-react";
+
+export default function Temp({ data }) {
+  const options = {
+    tooltip: {
+      trigger: "axis",
+      position: function (pt) {
+        return [pt[0], "10%"];
+      },
+    },
+    title: {
+      left: "center",
+      text: "Temperature Chart",
+    },
+    toolbox: {
+      feature: {
+        dataZoom: {
+          yAxisIndex: "none",
+        },
+        restore: {},
+        saveAsImage: {},
+      },
+    },
+    xAxis: {
+      type: "category",
+      boundaryGap: false,
+      data: data.map((item) => item.Time),
+    },
+    yAxis: {
+      type: "value",
+      boundaryGap: [0, "100%"],
+    },
+    dataZoom: [
+      {
+        type: "inside",
+        start: 0,
+        end: 10,
+      },
+      {
+        start: 0,
+        end: 10,
+      },
+    ],
+    series: [
+      {
+        name: "Temperature",
+        type: "line",
+        symbol: "none",
+        sampling: "lttb",
+        itemStyle: {
+          color: "rgb(255, 70, 131)",
+        },
+        areaStyle: {},
+        data: data.map((e) => e.Temperature),
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <ReactECharts option={options} />
+    </div>
+  );
+}
